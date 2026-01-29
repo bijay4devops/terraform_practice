8836e009a0134aea52e31fd8f235c9fbab7447a2
EC2 Default User <ec2-user@ip-172-31-5-209.ap-south-1.compute.internal>
2026-01-29 04:50:46 +0000

    add

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+ region = "ap-south-1"
+
+}
+
+resource "aws_instance" "exapmle" {
+ ami = var.ami_value
+ instance_type = var.instance_type_value
+
+}
---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+ami_value = "ami-0ff5003538b60d5ec"
+instance_type_value = "t2.micro"
+
